ed47d90c55c2cff6100608203238e5873268f0dc
Kaiqueandr <kaique.arlindo@magazineluiza.com.br>
2020-08-26 21:34:57 -0300

    Exercicio 7 pronto + sem calculo da média

--- lista1-exe7.js
+++ lista1-exe7.js
@@ -1,1 +1,20 @@
-console.log(`Exercício 7`)
+
+chefe()
+
+function chefe(){
+var vetor = [] //declara vetor
+exe7(vetor) // passando o vetor como parameteo - ESCOPO GLOBAL
+
+}
+
+function exe7(vet){ // vet representa vetor -> o que fizermos com vet, será feito em vetor
+var numero = Number(prompt(`Digite um número: `))
+do{
+    //colocar o numero em vet
+    vet.push(numero)
+    numero = Number(prompt(`Digite outro número, digite um número negativo para encerrar`))
+
+}
+while (numero >= 0)
+
+}
