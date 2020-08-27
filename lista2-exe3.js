930ab2a340c29caa4179a957c5608b684676b2b2
Kaiqueandr <kaique.arlindo@magazineluiza.com.br>
2020-08-26 22:23:02 -0300

    exercicio 3 lista 2 começo

---
+++ lista2-exe3.js
@@ -0,0 +1,27 @@
+
+chefe()
+
+function chefe(){
+    var vetor = []
+    entrada(vetor)
+    relat1(vetor)
+    relat2(vetor)
+    relat3(vetor)
+    relat4(vetor)
+}
+
+relat4(vet){
+
+}
+
+relat2(vet){
+
+}
+
+relat3(vet){
+
+}
+
+relat4(vet){
+
+}
