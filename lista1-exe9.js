4b120590037a0cea9c38596b2227eee2d49e5701
Kaiqueandr <kaique.arlindo@magazineluiza.com.br>
2020-08-26 22:02:35 -0300

    Exercicio 9 correto

--- lista1-exe9.js
+++ lista1-exe9.js
@@ -1,1 +1,26 @@
-console.log(`Exercício 9`)
+
+chefe()
+
+function chefe(){
+
+    var numero = Number(prompt(`Digite um número : `))
+    exe9(numero)
+}
+
+function exe9(num){ // Supondo que seja o número 6
+
+    var soma = 0
+    for(var divisor = num; divisor >= 1; divisor--){ // divisor começa em 6 e vai até o 1
+        if(num % divisor == 0){
+            soma = soma + divisor
+        }
+    }
+
+    /* 
+    for(var divisor = 1; divisor <= num; divisor++){ // divisor começa em 1 e vai até o 6
+        if(num % divisor == 0){
+            soma = soma + divisor
+        }
+    } */
+    console.log(`A soma dos divisores de ${num} é ${soma}`)
+}
